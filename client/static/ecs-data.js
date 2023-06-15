0c4cd376bbf1ba0d0ce23f67182ed5de344dca53
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-06-16 06:16:33 +0800

    Ready to role

--- client/static/ecs-data.js
+++ client/static/ecs-data.js
@@ -1,1 +1,1 @@
-window.ecs_data={world_addr:"0x77900f21764b5ac9a0b44d51175a824f14eebd79294154fcfa536efb18a3df4"};
+window.ecs_data={world_addr:"0x7f1d6c1b15e03673062d8356dc1174d5d85c310479ec49fe781e8bf89e4c4f8",rpc:"http://localhost:5050"};
